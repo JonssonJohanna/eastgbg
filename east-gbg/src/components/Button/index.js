2fa6cf8cae8ae7fc35f805861edaa795216f222c
JonssonJohannae <johanna_jonson@hotmail.com>
2022-04-20 11:58:15 +0200

    Created button

---
+++ east-gbg/src/components/Button/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./button.css";
+
+const Button = () => {
+  return <button className="btn">Click me</button>;
+};
+
+export default Button;
