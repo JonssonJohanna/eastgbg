1f288d269b004892ecafc87635171ae694859545
JonssonJohannae <johanna_jonson@hotmail.com>
2022-04-25 15:32:25 +0200

    Started adding useState, useEffect

--- east-gbg/src/App.js
+++ east-gbg/src/App.js
@@ -1,11 +1,16 @@
-/* import react, { useState, useEffect } from "react"; */
+import react, { useState, useEffect } from "react";
 // import "./App.css";
-/* import axios from "axios"; */
+
 import React from "react";
 import Button from "./components/Button";
 // import { Router } from "@reach/router";
 
 function App() {
+  const [fname, setFname] = React.useState();
+  const [sname, setSname] = React.useState();
+  useEffect(() => {
+    getMatch();
+  }, []);
   const options = {
     method: "GET",
     headers: {
@@ -13,18 +18,20 @@ function App() {
       "X-RapidAPI-Key": "5e354bc8acmsh9567dbb31f16aabp19e295jsnf6cac3b70ec4",
     },
   };
-
-  fetch(
-    "https://love-calculator.p.rapidapi.com/getPercentage?sname=Amanda&fname=Johanna",
-    options
-  )
-    .then((response) => response.json())
-    .then((response) => console.log(response))
-    .catch((err) => console.error(err));
+  const getMatch = async () => {
+    const response = await fetch(
+      `https://love-calculator.p.rapidapi.com/getPercentage?fname=${fname}&sname=${sname}`,
+      options
+    );
+    const data = await response.json();
+    console.log(data);
+    setFname(data.fname);
+  };
 
   return (
     <div>
-      <h1>{options.response}</h1>
+      <h2>hello {fname}</h2>
+      <h2>hello {sname}</h2>
       <Button>Click me</Button>
     </div>
   );
