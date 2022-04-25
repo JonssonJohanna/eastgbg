b4b10951accffde4cd96b74bbcb3b7f0db7e10b1
amandaprintz <fannyamandakarlsson@gmail.com>
2022-04-25 13:00:17 +0200

    Connected API

--- east-gbg/src/App.js
+++ east-gbg/src/App.js
@@ -1,10 +1,30 @@
 import logo from "./logo.svg";
 import "./App.css";
+import axios from "axios";
 // import { Router } from "@reach/router";
 import React from "react";
 import Button from "./components/Button";
 
 function App() {
+  const options = {
+    method: "GET",
+    url: "https://love-calculator.p.rapidapi.com/getPercentage",
+    params: { fname: "John", sname: "Alice" },
+    headers: {
+      "X-RapidAPI-Host": "love-calculator.p.rapidapi.com",
+      "X-RapidAPI-Key": "5e354bc8acmsh9567dbb31f16aabp19e295jsnf6cac3b70ec4",
+    },
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+
   return (
     <div>
       <h1>Hello</h1>
