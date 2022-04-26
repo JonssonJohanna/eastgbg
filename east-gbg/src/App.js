999f2b577a6ee42228f3e5bb88af928cfa9fbf58
JonssonJohannae <johanna_jonson@hotmail.com>
2022-04-26 19:05:56 +0200

    Created component

--- east-gbg/src/App.js
+++ east-gbg/src/App.js
@@ -1,52 +1,11 @@
 import React, { useState } from "react";
 import Button from "./components/Button";
+import Match from "./components/Match";
 
 function App() {
-  const [fname, setFname] = useState("Amanda");
-  const [sname, setSname] = useState("Johanna");
-
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Host": "love-calculator.p.rapidapi.com",
-      "X-RapidAPI-Key": "5e354bc8acmsh9567dbb31f16aabp19e295jsnf6cac3b70ec4",
-    },
-  };
-
-  const getMatch = async (e) => {
-    e.preventDefault();
-    const response = await fetch(
-      `https://love-calculator.p.rapidapi.com/getPercentage?fname=${fname}&sname=${sname}`,
-      options
-    );
-    const data = await response.json();
-    console.log(data);
-  };
-
   return (
-    <div>
-      <form>
-        <label>
-          Your name
-          <input
-            type="text"
-            value={fname}
-            onChange={(event) => setFname(event.target.value)}
-          />
-        </label>
-
-        <label>
-          Your lover's name
-          <input
-            type="text"
-            value={sname}
-            onChange={(event) => setSname(event.target.value)}
-          />
-        </label>
-        <button onClick={(e) => getMatch(e)}>Get Match</button>
-
-        {/*        <p>{fname + sname}</p> */}
-      </form>
+    <div className="app">
+      <Match />
     </div>
   );
 }
---
+++ east-gbg/src/components/Match/index.js
@@ -0,0 +1,52 @@
+import Button from "../Button";
+import React, { useState, useEffect } from "react";
+
+const Match = () => {
+  const [fname, setFname] = useState("Amanda");
+  const [sname, setSname] = useState("Johanna");
+
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Host": "love-calculator.p.rapidapi.com",
+      "X-RapidAPI-Key": "5e354bc8acmsh9567dbb31f16aabp19e295jsnf6cac3b70ec4",
+    },
+  };
+
+  const getMatch = async (e) => {
+    e.preventDefault();
+    const response = await fetch(
+      `https://love-calculator.p.rapidapi.com/getPercentage?fname=${fname}&sname=${sname}`,
+      options
+    );
+    const data = await response.json();
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <form>
+        <label>
+          Your name
+          <input
+            type="text"
+            value={fname}
+            onChange={(event) => setFname(event.target.value)}
+          />
+        </label>
+        <label>
+          Your lover's name
+          <input
+            type="text"
+            value={sname}
+            onChange={(event) => setSname(event.target.value)}
+          />
+        </label>
+        <Button handleClick={(e) => getMatch(e)}>Get Match /</Button>
+        {/*        <p>{fname + sname}</p> */}
+      </form>
+    </div>
+  );
+};
+
+export default Match;
